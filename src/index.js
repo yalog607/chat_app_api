2fc6262f4a2279ef7e77b1827aabe9e3b1863b31
Yalog <nguyenyalog@gmail.com>
2025-05-12 09:35:39 +0700

    add message model

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import { connectDB } from "../lib/db.js";
 dotenv.config();
@@ -9,6 +10,10 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
 import route from "../routes/index.js";
 route(app);
 
