d4235c1e70ea701e880dfc23bc11ed139a28b41e
Yalog <nguyenyalog@gmail.com>
2025-05-24 09:43:46 +0700

    config limit storage for json

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
